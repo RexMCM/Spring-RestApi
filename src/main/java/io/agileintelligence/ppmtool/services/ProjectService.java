e4be82a8fedce4ea38c9fad7f22ed351099991a5
Matheus Medeiros <matheus.cardosoibm.com@Matheuss-MacBook-Pro.local>
2021-02-22 15:03:16 -0300

    add validation, exceptions handler, etc

---
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -0,0 +1,35 @@
+package io.agileintelligence.ppmtool.services;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
+import io.agileintelligence.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase()+ "'Already Exist");
+        }
+
+    }
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Project ID does not exist");
+        }
+        return project;
+    }
+}
